d92bd1ef039758fe273a9b1179b7a14a2e066bd3
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2026-02-23 20:41:01 -0300

    init project

---
+++ project/api/api.dpr
@@ -0,0 +1,18 @@
+program api;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses Horse;
+
+begin
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse)
+    begin
+
+      Res.Send('pong');
+    end);
+
+  THorse.Listen(9000);
+end.
